180f8ed3e45f7fb8d1578b082de3c5f3cdab7f3e
take-tech1001 <tk.tech1001@gmail.com>
2021-01-25 03:58:23 +0900

    Added index.js to be used in image downloads

---
+++ index.js
@@ -0,0 +1,57 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+// 対象URL
+const url = ""
+
+// userAgent指定
+// PC
+const userAgent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C)';
+
+// iPhone
+// const userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F70 Safari/600.1.4';
+
+// Android
+// const userAgent = 'Mozilla/5.0 (Linux; Android 4.4.2; SH-01F Build/SA090) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36';
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.setUserAgent(userAgent);
+  // view portの設定
+  await page.setViewport({
+    width: 1200,
+    height: 800,
+  });
+  await page.goto(url);
+  await page.waitForTimeout(5000);
+
+  // 除去したい要素
+  await page.evaluate(() => {
+    document.querySelector('header').remove()
+    document.querySelector('footer').remove()
+  });
+  
+  const images = await page.evaluate(() => 
+    Array.from(document.querySelectorAll('img')).map(image => image.src)
+  )
+  const imageNames = await page.evaluate(() => 
+    Array.from(document.querySelectorAll('img')).map(image => image.src.split('/').pop())
+  )
+
+  for(let i = 0; i < images.length; i ++){
+    let localfilefullpath = path.join(__dirname, `/image/${imageNames[i]}`);
+
+    const viewSource = await page.goto(images[i]);
+    fs.writeFile(localfilefullpath, await viewSource.buffer(), (error) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      console.log(imageNames[i]);
+    });
+    await page.waitForTimeout(1000);
+  }
+  await browser.close();
+})();
